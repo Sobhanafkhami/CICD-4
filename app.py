1af9a2a1153611ec77e66885dd6a741d6b48810d
Sobhan Afkhami <sobhan.afkhami2@gmail.com>
2025-12-25 18:41:27 +0330

    feasdwda

---
+++ app.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI,HTTPException
+
+app = FastAPI()
+
+@app.get('/')
+def read_root():
+    return {'hello' : 'world'}
+
+@app.get('/book/{book_id}')
+def read_book(book_id: int):
+    if book_id > 10:
+        raise HTTPException(status_code=404,
+                            detail='Book not found')
+    return {'book_id': book_id}
